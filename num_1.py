05c37462134f630cbf77aedae72c132fe0264caa
KirillRg <75004961+KirillRg@users.noreply.github.com>
2020-12-02 13:36:26 +0300

    Create num_1.py

---
+++ num_1.py
@@ -0,0 +1,10 @@
+#задание 1
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+def min4(a, b, c, d):
+  return(min(a, b, c, d))
+print('Минимум равен:')
+print(min4(a, b, c, d))
